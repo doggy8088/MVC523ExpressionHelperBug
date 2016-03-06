106ec6559495a0c9d7a5bc109259ca3762f694bb
Will <doggy.huang@gmail.com>
2016-03-06 23:47:36 +0800

    Initial commit

---
+++ MVC523ExpressionHelperBug/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC523ExpressionHelperBug.Models;
+
+namespace MVC523ExpressionHelperBug.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var data = new List<Person>();
+
+            data.Add(new Person() { Id = 1, Name = "Will" });
+            data.Add(new Person() { Id = 2, Name = "Tom" });
+            data.Add(new Person() { Id = 3, Name = "Mary" });
+
+            return PartialView(data);
+        }
+    }
+}
---
+++ MVC523ExpressionHelperBug/Models/Person.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace MVC523ExpressionHelperBug.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        [DisplayName("姓名")]
+        public string Name { get; set; }
+    }
+}
